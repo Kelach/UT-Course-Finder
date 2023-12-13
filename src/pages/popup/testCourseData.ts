ea8c30af3e7ecf922fa4d844ff278a75916634d2
Kelechi Emeruwa <kelechi@utexas.edu>
2023-12-13 13:47:20 -0600

    added test data

---
+++ src/pages/popup/testCourseData.ts
@@ -0,0 +1,33 @@
+export const testCourseData =
+[
+    {
+        title : "Introduction to Computer Science",
+        number: "312",
+        department: "CS",
+        description:  "Introduction to computer science and programming. Problem solving using abstraction and decomposition. Introduction to object-oriented programming. Introduction to algorithms and data structures. Introduction to software engineering. Social and ethical issues in computing. The programming language used will be Python.",
+        prerequisites: "CS 311 is required"
+    }
+   
+]
+export const testCourseChartData = {
+    "A": 10,
+    "A-": 4,
+    "B+": 1,
+    "B": 7,
+    "B-": 12,
+    "C+": 3,
+    "C": 2,
+    "C-": 1,
+    "D+": 0,
+    "D": 0,
+    "D-": 0,
+    "F": 0,
+}
+
+// "CS 311",
+// "CS 312",
+// "CS 429",
+// "ASE 366K",
+// "CS 331",
+// "ASE 320",
+// "CS 314"
