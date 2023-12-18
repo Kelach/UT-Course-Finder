d1baf4f87cb1c723e3226a8fc0ba2e92f06060ff
Kelechi Emeruwa <kelechi@utexas.edu>
2023-12-18 00:23:33 -0600

    connected searchbar to real course data

--- src/pages/popup/SearchBar.tsx
+++ src/pages/popup/SearchBar.tsx
@@ -1,67 +1,78 @@
 import { useState } from 'react';
-import { Combobox, InputBase, Input, useCombobox, Group, Text } from '@mantine/core';
-import { CourseProps } from './Popup';
+import { Combobox, InputBase, useCombobox, Group, Text, CloseButton } from '@mantine/core';
+import { CourseOptionProps, CourseProps } from './Popup';
 import { useEffect } from 'react';
 
-function CourseOption(course: CourseProps) {
+function CourseOption(course: CourseOptionProps) {
   return (
-    <Group wrap={"nowrap"} preventGrowOverflow={true}>
-      <Text fz={18} fw={600}>{course.department}</Text>
+    <Group  wrap={"nowrap"} preventGrowOverflow={true}>
+      <Text miw={30} fz={18} fw={600}>{course.department}</Text>
       <div>
         <Text fz={"sm"}>{course.number}</Text>
-        <Text truncate={"end"} fz={15}>{course.title}</Text>
+        <Text maw={200} truncate={"end"} fz={15}>{course.title}</Text>
       </div>
     </Group>
   )
 }
 
-export default function SearchBar({courseData, setCourse} : {courseData : CourseProps[], setCourse: React.Dispatch<React.SetStateAction<CourseProps | null>>}) {
+export default function SearchBar({setCourse}: {setCourse: React.Dispatch<React.SetStateAction<CourseProps | null>>}) {
 
+  const [courseSuggestions, setCourseSuggestions] = useState<CourseOptionProps[]>([]);
+  const [search, setSearch] = useState('');
   const combobox = useCombobox({
     onDropdownClose: () => combobox.resetSelectedOption(),
   });
 
-  const [value, setValue] = useState<string | null>(null);
-  const [search, setSearch] = useState('');
+  useEffect(() => {
+    chrome.runtime.sendMessage({message: "CourseSuggestions"}, (response) => {
+      setCourseSuggestions(response.data);
+    })
+  }, [])
 
   // filtering 
-  const shouldFilterOptions = courseData.every((item) => item.title !== search || item.description !== search);
+  const shouldFilterOptions = courseSuggestions.every((item) => item.title !== search || item.number !== search);
   const filteredOptions = shouldFilterOptions
-    ? courseData.filter((course : CourseProps) => course.title.toLowerCase().includes(search.toLowerCase().trim())
+    ? courseSuggestions.filter((course : CourseOptionProps) => course.title.toLowerCase().includes(search.toLowerCase().trim())
     || `${course.department} ${course.number}`.toLowerCase().includes(search.toLowerCase().trim()))
-    : courseData;
+    : courseSuggestions;
+
 // setting search options
-  const options = filteredOptions.map((course: CourseProps) => (
-    <Combobox.Option value={`${course.department} ${course.number}: ${course.title}`} key={`${course.title}:${course.number}`}>
-      <CourseOption {...course} />
+  const options = filteredOptions.map((courseOption: CourseOptionProps, index) => (
+    <Combobox.Option value={`${JSON.stringify(courseOption)}`} key={`${courseOption.id}`}>
+      <CourseOption {...courseOption} />
     </Combobox.Option>
   ));
-
+  
   // selects first option
   useEffect(() => {
     combobox.selectFirstOption();
   }, [search])
   
   // on submit handler
-  const onOptionSubmitHandler = (courseOption : string) => {
-    setSearch(courseOption);
+  const onOptionSubmitHandler = (courseOptionJSONString : string) => {
+    let courseOption = JSON.parse(courseOptionJSONString) as CourseOptionProps
+    setSearch( `${courseOption.department} ${courseOption.number}`);
     // setting new course
-    const course = courseData.find((item : CourseProps) => courseOption === `${item.department} ${item.number}: ${item.title}`);
-    if (course) {
-      setCourse(course);
-    }else{ console.log("error: tried to handle course option that was not found", course)}
-    combobox.closeDropdown(); // closes dropdown
+    chrome.runtime.sendMessage({message: "CourseInfo", courseID: courseOption.id}, (response) => {
+      if (response.data){
+        setCourse(response.data);
+      } else{ console.log("error: tried to handle course option that was not found", response.data)}
+      // closes dropdown
+      combobox.closeDropdown();
+    })
   }
+
   return (
     <Combobox
       store={combobox}
       withinPortal={false}
       onOptionSubmit={onOptionSubmitHandler}
+      position='bottom'
       >
       <Combobox.Target>
         <InputBase
         size='md'
-        w={"50vw"}
+        w={"75vw"}
       radius={"xl"}
           // rightSection={<Combobox.Search />}
           value={search}
@@ -74,15 +85,25 @@ export default function SearchBar({courseData, setCourse} : {courseData : Course
           onFocus={() => combobox.openDropdown()}
           onBlur={() => {
             combobox.closeDropdown();
-            setSearch(value || "");
+            setSearch(search || "");
           }}
           placeholder="E.g. CS 349"
-          rightSectionPointerEvents="none"
+          // rightSectionPointerEvents="none"
+          rightSection={
+            search !== '' && (
+              <CloseButton
+                size="sm"
+                onMouseDown={(event) => event.preventDefault()}
+                onClick={() => {setSearch(''); setCourse(null)}} // clears search
+                aria-label="Clear value"
+              />
+            )
+          }
         />
       </Combobox.Target>
 
       <Combobox.Dropdown>
-        <Combobox.Options >
+        <Combobox.Options mah={350} style={{overflowY: 'auto'}} >
           {options.length > 0 ? options : <Combobox.Empty>No Course Found :/</Combobox.Empty>}
         </Combobox.Options>
       </Combobox.Dropdown>
