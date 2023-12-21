0f4dd3c0f31a0ac22dbc2181a0d5118748706a89
Kelechi Emeruwa <kelechi@utexas.edu>
2023-12-20 22:04:57 -0600

    added dark/light themes

--- src/pages/popup/index.tsx
+++ src/pages/popup/index.tsx
@@ -7,18 +7,26 @@ import refreshOnUpdate from 'virtual:reload-on-update-in-view';
 import { MantineProvider, createTheme } from '@mantine/core';
 
 refreshOnUpdate('pages/popup');
+const theme  = createTheme({
+  primaryShade: {light: 5, dark: 8},
+  primaryColor: 'orange',
+  fontFamily: 'Roboto',
+  headings: {
+    fontFamily: 'Roboto',
+  },
+  components: { "badge" :{
+    
+  } },
+})
 
 function init() {
   const appContainer = document.querySelector('#app-container');
   if (!appContainer) {
     throw new Error('Can not find #app-container');
   }
-  // const theme = createTheme({
-
-  // })
   const root = createRoot(appContainer);
   root.render(
-    <MantineProvider>
+    <MantineProvider theme={theme} defaultColorScheme='auto'>
       <Popup />
     </MantineProvider>,
   );
